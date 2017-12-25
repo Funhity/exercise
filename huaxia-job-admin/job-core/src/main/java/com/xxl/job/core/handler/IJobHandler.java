0755a742cb4ab4f7ce7a9348783e3bbc191965c7
xiaogui <xiaogui@huaxiafinance.com>
2017-12-26 06:18:20 +0800

    'initial'

---
+++ huaxia-job-admin/job-core/src/main/java/com/xxl/job/core/handler/IJobHandler.java
@@ -0,0 +1,28 @@
+package com.xxl.job.core.handler;
+
+import com.xxl.job.core.biz.model.ReturnT;
+
+/**
+ * job handler
+ *
+ * @author xuxueli 2015-12-19 19:06:38
+ */
+public abstract class IJobHandler {
+
+	/** success */
+	public static final ReturnT<String> SUCCESS = new ReturnT<String>(200, null);
+	/** fail */
+	public static final ReturnT<String> FAIL = new ReturnT<String>(500, null);
+	/** fail retry */
+	public static final ReturnT<String> FAIL_RETRY = new ReturnT<String>(501, null);
+
+
+	/**
+	 * job handler
+	 * @param params
+	 * @return
+	 * @throws Exception
+	 */
+	public abstract ReturnT<String> execute(String... params) throws Exception;
+	
+}
